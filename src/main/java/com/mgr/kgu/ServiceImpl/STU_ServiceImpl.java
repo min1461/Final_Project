fd36326d7a3a7c2a3138455105f514ce71ead8b3
min1461 <min1461@naver.com>
2021-04-08 20:16:30 +0900

    Service,DAO 변경

--- src/main/java/com/mgr/kgu/ServiceImpl/STU_ServiceImpl.java
+++ src/main/java/com/mgr/kgu/ServiceImpl/STU_ServiceImpl.java
@@ -8,23 +8,23 @@ import com.mgr.kgu.Service.STU_Service;
 import com.mgr.kgu.VO.STU_VO;
 
 @Service
-public class STU_ServiceImpl implements STU_DAO {
+public class STU_ServiceImpl implements STU_Service {
 
 	@Autowired
-	private STU_Service STU_Service;
+	private STU_DAO STU_DAO;
 
 	@Override
 	public STU_VO getTelinfo() {
-		return STU_Service.getTelinfo();
+		return STU_DAO.getTelinfo();
 	}
 	
 	@Override
 	public STU_VO getSTU_NUM() {
-		return STU_Service.getSTU_NUM();
+		return STU_DAO.getSTU_NUM();
 	}
 	
 	@Override
 	public STU_VO getSTU_PW() {
-		return this.STU_Service.getSTU_PW();
+		return this.STU_DAO.getSTU_PW();
 	}
 }
