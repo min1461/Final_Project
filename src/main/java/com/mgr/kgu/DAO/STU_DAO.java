a2daf3ebdc3753317f2cbee08bf9bc08a11c32a5
min1461 <min1461@naver.com>
2021-04-09 15:04:45 +0900

    stu 데이터 연동 관련 전체 변경 + Integer ==> int로 수정

--- src/main/java/com/mgr/kgu/DAO/STU_DAO.java
+++ src/main/java/com/mgr/kgu/DAO/STU_DAO.java
@@ -5,6 +5,6 @@ import com.mgr.kgu.VO.STU_VO;
 // 인터페이스
 public interface STU_DAO {
 	boolean getlogininfo(Integer STU_NUM, String STU_PW);
-	STU_VO getAllinfo(Integer STU_NUM);
+	STU_VO getAllinfo(int STU_NUM);
 
 }
